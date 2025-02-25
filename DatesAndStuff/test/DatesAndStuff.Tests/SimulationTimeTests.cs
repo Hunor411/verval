1ccb2b41d9c4275853f6f6dbfd1feaebdaa295a9
Deak Hunor <deakhunor2002@gmail.com>
2025-02-25 22:22:06 +0200

    rename methods

--- DatesAndStuff/test/DatesAndStuff.Tests/SimulationTimeTests.cs
+++ DatesAndStuff/test/DatesAndStuff.Tests/SimulationTimeTests.cs
@@ -26,7 +26,7 @@ namespace DatesAndStuff.Tests
 
         [Test]
         // Default time is not current time.
-        public void SimulationTime_Construction()
+        public void SimulationTime_DefaultTime_IsNotCurrentTime()
         {
             throw new NotImplementedException();
         }
@@ -42,7 +42,7 @@ namespace DatesAndStuff.Tests
         // >= same
         // max
         // min
-        public void SimulationTime_Op()
+        public void GivenTwoSimulationTimes_WhenCompared_ThenComparisonIsCorrect()
         {
             throw new NotImplementedException();
         }
@@ -55,7 +55,7 @@ namespace DatesAndStuff.Tests
             // add
             // substract
             // Given_When_Then
-            public void Addition_SimulationTimeIsShifted()
+            public void SimulationTime_Addition_ShiftsTimeCorrectly()
             {
                 // UserSignedIn_OrderSent_OrderIsRegistered
                 // DBB, specflow, cucumber, gherkin
@@ -76,7 +76,7 @@ namespace DatesAndStuff.Tests
 
             [Test]
             //Method_Should_Then
-            public void Subtracttion_SimulationTimeShifted()
+            public void GivenSimulationTime_WhenSubtracted_ThenTimeIsShiftedBack()
             {
                 // code kozelibb
                 // RegisterOrder_SignedInUserSendsOrder_OrderIsRegistered
@@ -87,14 +87,14 @@ namespace DatesAndStuff.Tests
 
         [Test]
         // simulation difference timespane and datetimetimespan is the same
-        public void SimulationTime_SubtractSimulationTime()
+        public void GivenSimulationTime_WhenMillisecondsAreAdded_ThenTimeIncreases()
         {
             throw new NotImplementedException();
         }
 
         [Test]
         // millisecond representation works
-        public void SimulationTime_1msPerTick()
+        public void SimulationTime_MillisecondRepresentation_WorksCorrectly()
         {
             //var t1 = SimulationTime.MinValue.AddMilliseconds(10);
             throw new NotImplementedException();
@@ -102,7 +102,7 @@ namespace DatesAndStuff.Tests
 
         [Test]
         // next millisec calculation works
-        public void SimulationTime_NextMillisec()
+        public void GivenSimulationTime_WhenNextMillisecCalled_ThenReturnsIncrementedTime()
         {
             //Assert.AreEqual(t1.TotalMilliseconds + 1, t1.NextMillisec.TotalMilliseconds);
             throw new NotImplementedException();
@@ -110,14 +110,14 @@ namespace DatesAndStuff.Tests
 
         [Test]
         // creat a SimulationTime from a DateTime, add the same milliseconds to both and check if they are still equal
-        public void SimulationTime_AddMilliSeconds()
+        public void SimulationTime_AddMilliseconds_UpdatesTimeCorrectly()
         {
             throw new NotImplementedException();
         }
 
         [Test]
         // the same as before just with seconds
-        public void SimulationTime_AddSeconds()
+        public void GivenSimulationTime_WhenSecondsAdded_ThenTimeUpdatesCorrectly()
         {
             throw new NotImplementedException();
         }
@@ -131,7 +131,7 @@ namespace DatesAndStuff.Tests
 
         [Test]
         // check string representation given by ToString
-        public void SimulationTime_ToString()
+        public void GivenSimulationTime_WhenToStringCalled_ThenReturnsCorrectFormat()
         {
             throw new NotImplementedException();
         }
