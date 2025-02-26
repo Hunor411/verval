989119a2e7c00eef05d94f6d67e76b1bb5b3ef5a
Deak Hunor <deakhunor2002@gmail.com>
2025-02-26 19:37:28 +0200

    4. 4-4 unit test

--- DatesAndStuff/test/DatesAndStuff.Tests/PersonTests.cs
+++ DatesAndStuff/test/DatesAndStuff.Tests/PersonTests.cs
@@ -61,7 +61,7 @@ public class PersonTests
             Assert.IsTrue(task.IsFaulted);
         }   
     }
-
+    
     [TestFixture]
     public class SalaryTests
     {
@@ -75,25 +75,48 @@ public class PersonTests
         [Test]
         public void PositiveIncrease_ShouldIncreaseSalary()
         {
-            throw new NotImplementedException();
+            double initialSalary = sut.Salary;
+            sut.IncreaseSalary(10);
+            
+            Assert.IsTrue(sut.Salary > initialSalary, "Salary should increase when given a positive percentage.");
         }
 
         [Test]
         public void ZeroPercentIncrease_ShouldNotChangeSalary()
         {
-            throw new NotImplementedException();
+            double initialSalary = sut.Salary;
+            sut.IncreaseSalary(0);
+
+            double newSalary = sut.Salary;
+            
+            Assert.AreEqual(initialSalary, newSalary, "A 0% increase should not change the salary.");
         }
 
         [Test]
         public void NegativeIncrease_ShouldDecreaseSalary()
         {
-            throw new NotImplementedException();
+            // Arrange
+            double initialSalary = sut.Salary;
+            
+            // Act
+            sut.IncreaseSalary(-5);
+            double newSalary = sut.Salary;
+            
+            // Assert
+            Assert.That(newSalary, Is.LessThan(initialSalary), "A negative salary increase should decrease the salary.");
         }
 
         [Test]
         public void SmallerThanMinusTenPercent_ShouldFail()
         {
-            throw new NotImplementedException();
+            // Arrange
+            double initialSalary = sut.Salary;
+            
+            // Act
+            TestDelegate action = () => sut.IncreaseSalary(-15);
+            
+            // Assert
+            Assert.That(action, Throws.TypeOf<ArgumentOutOfRangeException>(), "Increasing salary by more than -10% should throw an exception.");
         }
     }
 }
--- DatesAndStuff/test/DatesAndStuff.Tests/SimulationTimeTests.cs
+++ DatesAndStuff/test/DatesAndStuff.Tests/SimulationTimeTests.cs
@@ -121,32 +121,56 @@ public class SimulationTimeTests
     {
         [Test]
         // simulation difference timespane and datetimetimespan is the same
-        public void GivenSimulationTime_WhenMillisecondsAreAdded_ThenTimeIncreases() 
+        public void GivenSimulationTime_WhenMillisecondsAreAdded_ThenTimeIncreases()
         {
-            throw new NotImplementedException();
+            var baseTime = new SimulationTime(2025, 1, 1, 0, 0, 0);
+            var updatedTime = baseTime.AddMilliseconds(1000);
+            
+            Assert.AreNotEqual(baseTime, updatedTime, "Adding milliseconds should change the SimulationTime.");
         }
         
         [Test]
         // millisecond representation works
         public void SimulationTime_MillisecondRepresentation_WorksCorrectly()
         {
-            //var t1 = SimulationTime.MinValue.AddMilliseconds(10);
-            throw new NotImplementedException();
+            // Arrange
+            var baseTime = SimulationTime.MinValue;
+            var expectedTime = baseTime.AddMilliseconds(10);
+
+            // Act
+            var actualTime = SimulationTime.MinValue + TimeSpan.FromMilliseconds(10);
+
+            // Assert
+            Assert.AreEqual(expectedTime, actualTime, "Adding 10 milliseconds should correctly update the SimulationTime.");
         }
         
         [Test]
         // next millisec calculation works
         public void GivenSimulationTime_WhenNextMillisecCalled_ThenReturnsIncrementedTime()
         {
-            //Assert.AreEqual(t1.TotalMilliseconds + 1, t1.NextMillisec.TotalMilliseconds);
-            throw new NotImplementedException();
+            // Arrange
+            var time = new SimulationTime(2025, 1, 1, 1, 0, 0);
+
+            // Act
+            var nextMiliseconds = time.NextMillisec;
+
+            // Assert
+            Assert.That(nextMiliseconds.TotalMilliseconds, Is.EqualTo(time.TotalMilliseconds + 1));
         }
         
         [Test]
         // creat a SimulationTime from a DateTime, add the same milliseconds to both and check if they are still equal
         public void SimulationTime_AddMilliseconds_UpdatesTimeCorrectly()
         {
-            throw new NotImplementedException();
+            // Arrenge
+            var baseTime = new SimulationTime(2025, 1, 1, 1, 0, 0);
+            var expectedTime = baseTime.AddMilliseconds(500);
+            
+            // Act
+            var actualTime = baseTime + TimeSpan.FromMilliseconds(500);
+
+            // Assert
+            Assert.That(actualTime, Is.EqualTo(expectedTime));
         }
         
         [Test]
